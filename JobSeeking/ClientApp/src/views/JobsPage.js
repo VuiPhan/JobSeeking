0cebd13f3d6c63716951656e44d6f56000762ec9
vuiphan <phanvui453@gmail.com>
2020-10-25 22:33:49 +0700

    Degisn job page

--- JobSeeking/ClientApp/src/views/JobsPage.js
+++ JobSeeking/ClientApp/src/views/JobsPage.js
@@ -17,6 +17,13 @@ import LGCompanyPage from "Language/CompanyPage";
 import ListViewKendo from "components/ListViewKendo/ListViewKendo.js";
 import JobsApi from "api/Company/JobsAPI.js";
 import { useHistory, useParams } from "react-router-dom";
+import { Icon } from "@material-ui/core";
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import PeopleIcon from '@material-ui/icons/People';
+import DateRangeIcon from '@material-ui/icons/DateRange';
 const useStyles = makeStyles(styles);
 
 export default function JobsPage(props) {
@@ -31,7 +38,7 @@ export default function JobsPage(props) {
   );
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   var parse = require('html-react-parser');
-  const [data, setData] = useState({companyName:'' ,TimeWorking:'',jobsTitle: '', jobDescriptions: 'a', jobRequirements: 'b', reasonsToJoin: 'c', loveWorkingHere: 'd' });
+  const [data, setData] = useState({ companyName: '', TimeWorking: '', jobsTitle: '', jobDescriptions: 'a', jobRequirements: 'b', reasonsToJoin: 'c', loveWorkingHere: 'd' });
   const history = useHistory();
   const { jobID } = useParams();
   useEffect(() => {
@@ -41,7 +48,7 @@ export default function JobsPage(props) {
       console.log(result[0]);
     }
     fetchMyAPI()
-  }, []);
+  }, [jobID]);
 
 
   return (
@@ -59,44 +66,85 @@ export default function JobsPage(props) {
                     <h5><LocationOnIcon></LocationOnIcon> {data.companyAddress}</h5>
                     <h5><AccessAlarmIcon></AccessAlarmIcon > {data.timeWorking}</h5>
                   </div>
+
+                </div>
+                <div className="containerImage">
+                  <img src="https://cdn.itviec.com/photos/39011/jd_photo_thumbnail/jd_photo_thumbnail-1537273.jpg?TKHL8117CQAD1rE2g5Gp8G5J"></img>
+                  <img src="https://cdn.itviec.com/photos/39012/jd_photo_thumbnail/jd_photo_thumbnail-1537276.jpg?AuQcKevkfh3PeLopGrhReQfZ"></img>
+                  <img src="https://cdn.itviec.com/photos/39013/jd_photo_thumbnail/jd_photo_thumbnail-1537274.jpg?pEASJFrheg44uQmxpgkhudc4"></img>
                 </div>
               </GridItem>
             </GridContainer>
-            <div >
-              <h3>{parse(data.jobsTitle)}</h3>
-              <h3>
-                {res.LyDoNenGiaNhap}
-              </h3>
-              <div>
-                {parse(data.reasonsToJoin)}
+            <div className="row">
+              <div className="side">
+                <div className="detail_side">
+                  <AccessAlarm />
+                  <p className="detail_side_content">Không có OT</p>
+                </div>
+                <div className="detail_side">
+                <MonetizationOnIcon color="secondary" />
+                  <p className="detail_side_content"> Mức lương: 700 - 1300$</p>
+                </div>
+
+                <div className="detail_side">
+                <LocationCityIcon color="secondary" />
+                  <p className="detail_side_content"> Ho Chi Minh</p>
+                </div>
+                <br></br>
+
+                <div className="detail_side">
+                <Brightness7Icon />
+                  <p className="detail_side_content"> Sản phẩm</p>
+                </div>
+                <br></br>
+                <div className="detail_side">
+                <PeopleIcon  />
+                  <p className="detail_side_content"> 300 - 500</p>
+                </div>
+                <br></br>
+                <div className="detail_side">
+                <DateRangeIcon  />
+                  <p className="detail_side_content"> 5 ngày trước</p>
+                </div>
+
               </div>
-              <h3>
-                {res.MoTaCongViec}
-              </h3>
-              <div>
-                {parse(data.jobDescriptions)}
+              <div className="main">
+                <h3>{parse(data.jobsTitle)}</h3>
+                <h3>
+                  {res.LyDoNenGiaNhap}
+                </h3>
+                <div>
+                  {parse(data.reasonsToJoin)}
+                </div>
+                <h3>
+                  {res.MoTaCongViec}
+                </h3>
+                <div>
+                  {parse(data.jobDescriptions)}
+                </div>
+
+                <h3>
+                  {res.YeuCauCongViec}
+                </h3>
+                <div>
+                  {parse(data.jobRequirements)}
+
+                </div>
+                <h3>
+                  {res.TaiSaoBanYeuThich}
+                </h3>
+                <div>
+                  {parse(data.loveWorkingHere)}
+                </div>
+
               </div>
 
-              <h3>
-                {res.YeuCauCongViec}
-              </h3>
-              <div>
-                {parse(data.jobRequirements)}
 
-              </div>
-              <h3>
-                {res.TaiSaoBanYeuThich}
-              </h3>
-              <div>
-                {parse(data.loveWorkingHere)}
-              </div>
-              <h3>
-                {res.ViecLamPhuHop}
-              </h3>
-              <ListViewKendo>
-
-              </ListViewKendo>
             </div>
+            <h3>
+              {res.ViecLamPhuHop}
+            </h3>
+            <ListViewKendo></ListViewKendo>
           </div>
         </div>
       </div>
