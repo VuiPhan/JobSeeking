ae69cae8a8da3469e129c0f687ccf3fd65dc2a50
vuiphan <phanvui453@gmail.com>
2020-12-30 19:54:25 +0700

    done

--- JobSeeking/ClientApp/src/components/HeaderApp/HeaderApp.js
+++ JobSeeking/ClientApp/src/components/HeaderApp/HeaderApp.js
@@ -53,7 +53,7 @@ function HeaderApp(props) {
         <Parallax small filter image={require("../../assets/img/Home2.jpg")} />
         <div style={{ display: 'flex' }}>
           <div style={{ height: 160, zIndex: 99, width: 800, position: 'relative', justifyContent: 'center', top: -264, margin: 'auto' }}>
-            <h3 style={{ color: "white", paddingBottom: 20 }}>{countJob} Việc Làm IT Chất Dành Cho Bạn</h3>
+            <h3 style={{ color: "white", paddingBottom: 20 }}>{countJob}+ Việc Làm IT Chất Dành Cho Bạn</h3>
             <SelectGroup></SelectGroup>
           </div>
         </div>
